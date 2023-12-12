537166f9f9e3721dbfd4bf0bcb55c510375f3c8b
cm <chanmi125128@gmail.com>
2023-12-12 20:19:48 +0900

    develop commit

---
+++ src/main/java/com/cwm/develop/DevelopApplication.java
@@ -0,0 +1,13 @@
+package com.cwm.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwm/develop/DevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
