f2756982d85f04327ade676363c93d33dad237cc
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-12-29 02:20:42 +0900

    Update OAuth2LoginSuccessHandler.java

--- src/main/java/com/cwm/develop/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/cwm/develop/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -31,10 +31,10 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 
             // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
             // User의 Role이 곧장 USER로 들어가야함. 메인 페이지로 리다이렉트 해야하지 않나 생각됨
-            if(oAuth2User.getRole() == Role.USER) {
+            if(oAuth2User.getRole() == Role.GUEST) {
                 String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
                 response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-                // response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
+                response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
 
                 jwtService.sendAccessAndRefreshToken(response, accessToken, null);
 //                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
