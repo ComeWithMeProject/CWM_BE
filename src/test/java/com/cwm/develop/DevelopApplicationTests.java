4daf899f7846ca4ec8f56c21d329b99f163b0787
Cheon DaYeon <119835857+cdayeon@users.noreply.github.com>
2023-12-29 01:48:09 +0900

    Update DevelopApplicationTests.java

--- src/test/java/com/cwm/develop/DevelopApplicationTests.java
+++ src/test/java/com/cwm/develop/DevelopApplicationTests.java
@@ -1,20 +1,13 @@
 package com.cwm.develop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.cwm.develop.user.service.UserService;
 
 @SpringBootTest
 class DevelopApplicationTests {
 
-    @Autowired
-    private UserService userService; // UserService를 주입받음
-
-    @Test
-    void contextLoads() {
-        // UserService를 사용하여 회원 가입 또는 로그인과 같은 기능을 테스트하는 코드 작성
-        // 예를 들어, userService의 메서드를 호출하고 반환값을 검증하는 코드를 작성할 수 있습니다.
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
