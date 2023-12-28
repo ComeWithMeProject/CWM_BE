161c7fc5dc7434d9f311314b00f3425c38e40547
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-12-29 00:14:56 +0900

    Update DevelopApplicationTests.java

--- src/test/java/com/cwm/develop/DevelopApplicationTests.java
+++ src/test/java/com/cwm/develop/DevelopApplicationTests.java
@@ -3,6 +3,7 @@ package com.cwm.develop;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.cwm.develop.user.service.UserService;
 
 @SpringBootTest
 class DevelopApplicationTests {
