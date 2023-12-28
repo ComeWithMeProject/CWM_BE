69d0fdd7aaa42baa1b4e1b14db03e34a7af4e164
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-12-29 02:18:06 +0900

    Update OAuth2LoginSuccessHandler.java

--- src/main/java/com/cwm/develop/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/cwm/develop/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -34,7 +34,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
             if(oAuth2User.getRole() == Role.USER) {
                 String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
                 response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-                response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
+                // response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
 
                 jwtService.sendAccessAndRefreshToken(response, accessToken, null);
 //                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
