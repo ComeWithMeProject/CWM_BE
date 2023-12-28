2615d44742b8315bb0ecab96fd42e45e8777def2
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-12-29 02:17:05 +0900

    Update LoginSuccessHandler.java

--- src/main/java/com/cwm/develop/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/cwm/develop/global/login/handler/LoginSuccessHandler.java
@@ -5,22 +5,24 @@ import com.cwm.develop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.web.DefaultRedirectStrategy;
 
 @Slf4j
 @RequiredArgsConstructor
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    /* 커스텀 JSON 로그인 필터를 정상적으로 통과하여 인증 처리가 되었을 때(=로그인 성공)
+     * 로그인 성공 핸들러가 동작한다.
+     * SimpleUrlAuthenticationSuccessHandler를 상속받아 구현했다.
+     */
+
     private final JwtService jwtService;
     private final UserRepository userRepository;
-    private final RedisTemplate<String, String> redisTemplate;
 
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
@@ -28,22 +30,11 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
-        String email = extractUsername(authentication);
-        String accessToken = jwtService.createAccessToken(email);
-        String refreshToken = jwtService.createRefreshToken();
+        String email = extractUsername(authentication); //인증 정보에서 Username(email) 추출
+        String accessToken = jwtService.createAccessToken(email); //JwtService의 createAccessToken을 사용하여 AccessToken 발급
+        String refreshToken = jwtService.createRefreshToken(); //JwtService의 createRefreshToken을 사하여 RefreshToken 발급
 
-        if (email != null && accessToken != null) {
-            redisTemplate.opsForValue().set(email, accessToken);
-        } else {
-            log.error("Email 또는 AccessToken 값이 null입니다. Redis에 저장하지 않습니다.");
-            return;
-        }
-
-        // JWT를 클라이언트에게 전송
-        response.setHeader("Authorization", "Bearer " + accessToken);
-
-        // RefreshToken도 JWT로 생성하여 클라이언트에게 전송
-        response.setHeader("Authorization-refresh", "Bearer " + refreshToken);
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); //응답 헤더에 accessToken, refreshToken 실어서 응답
 
         userRepository.findByEmail(email)
                 .ifPresent(user -> {
@@ -52,18 +43,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
                 });
         log.info("로그인에 성공했습니다. 이메일 : {}", email);
         log.info("로그인에 성공했습니다. AccessToken : {}", accessToken);
-        log.info("로그인에 성공했습니다. RefreshToken : {}", refreshToken);
         log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
-
-        // 리다이렉션을 위한 기본 대상 URL 설정
-        setDefaultTargetUrl("/api/areaBasedList1/main?page=1&size=12");
-
-        // RedirectStrategy를 사용하여 리다이렉션 수행
-        try {
-            new DefaultRedirectStrategy().sendRedirect(request, response, getDefaultTargetUrl());
-        } catch (Exception e) {
-            log.error("리다이렉션 중 오류 발생: {}", e.getMessage());
-        }
     }
 
     private String extractUsername(Authentication authentication) {
